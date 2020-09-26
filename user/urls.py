4abc65af1af9f84251ef942e6a6f675b0e6797da
koushal2001 <bhatkoushal2001@gmail.com>
2020-09-26 15:14:16 +0530

    Integrated Pages

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.welcome,name='welcome'),
     path('register', views.usersignup,name='register'),
     path('login', views.loginn,name='login'),
-    path('logout', views.logout,name='logout'),
+    path('answer', views.answer,name='answer'),
+    path('logout', views.logoutt,name='logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, authenticate ,logout
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Profile
@@ -20,7 +20,7 @@ def usersignup(request) :
             print("efe")
             login(request, user)
             print("sde")
-            return render (request, 'user/success.html')
+            return render (request, 'user/Answer.html')
 
         except :
             return render (request, 'user/home.html')
@@ -37,7 +37,7 @@ def loginn(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return render(request,"users/success.html")
+            return render(request,"users/logout.html")
         else:
             return render(request,"users/failure.html")
     elif request.method == 'GET':
@@ -45,27 +45,8 @@ def loginn(request):
     return render(request, "user/login.html")
 
 def answer(request):
-    print("skdhguw")
-    if request.method=='POST':
-        print("skdhguw11")
-        one=request.POST.get('first')
-        two = request.POST.get('second')
-        three = request.POST.get('three')
-        four = request.POST.get('fourth')
-        five = request.POST.get('fifth')
-        six = request.POST.get('six')
-        seven = request.POST.get('seven')
-        eight = request.POST.get('eight')
-        nine = request.POST.get('nine')
-        ten = request.POST.get('ten')
-        eleven = request.POST.get('eleven')
-        twelve = request.POST.get('twelve')
-        total_score = one + two + three+ four+five+six+seven +eight + nine+ten+eleven+twelve
-        return render (request,'user/sucess.html',context={'total_score' : total_score})
-    if request.method == 'GET':
-        print("skdhguw")
-        return render(request,'user/sucess.html')
-    return render(request, 'user/sucess.html')
-
-def logout(request):
     return render(request, 'user/logout.html')
+
+def logoutt(request):
+    logout(request)
+    return redirect('login')
